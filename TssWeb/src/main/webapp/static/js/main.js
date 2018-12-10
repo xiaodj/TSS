ecd3cf06c3dd196e882aecfc53862bb96ff358f9
xiaodj <xiaodj20140808@163.com>
2018-12-11 03:13:59 +0800

    处理记录接口

--- TssWeb/src/main/webapp/static/js/main.js
+++ TssWeb/src/main/webapp/static/js/main.js
@@ -10,46 +10,131 @@ layui.define(['layer','form', 'element', 'table', 'laypage', 'layer'],function (
     var layer = layui.layer;
     var $ = layui.$;
 
+    var page = document.getElementById("page");
     element.on('nav(main)', function (data) {
         if (data.context.id == "1"){
-            document.getElementById("page").innerHTML="1";
+            page.innerHTML="";
+            var tbEmn = document.createElement("table");
+            tbEmn.className = "layui-hide";
+            tbEmn.id = "RealTime";
+            page.appendChild(tbEmn);
+
+            var RTData = [
+                {"username":"张三", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"},
+                {"username":"李四", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"},
+                {"username":"王五", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"}
+                ];
+
+            table.render({
+                elem: '#RealTime',
+                data:RTData,
+                cols: [[
+                    {field:'id', width:'10%', title: '序号', type:'numbers'}
+                    ,{field:'username', width:'15%', title: '姓名'}
+                    ,{field:'sex', width:'15%', title: '进入时间'}
+                    ,{field:'city', width:'20%', title: '绿卡到期日期'}
+                    ,{field:'experience', width:'20%', title: '密卡到期日期'}
+                    ,{field:'score', width:'20%', title: 'CP到期日期'}
+                ]]
+                ,page: true
+            });
+
         }else if (data.context.id == "2"){
-            document.getElementById("page").innerHTML="2";
+            page.innerHTML="";
+            var html = '<script type="text/html" id="toolbarDemo">\n' +
+                '  <div class="layui-btn-container">\n' +
+                '    <button class="layui-btn layui-btn-sm" lay-event="getCheckData">获取选中行数据</button>\n' +
+                '  </div>\n' +
+                '</script>';
+            page.innerHTML = html;
+            var tbEmn = document.createElement("table");
+            tbEmn.className = "layui-hide";
+            tbEmn.id = "RealTime";
+            page.appendChild(tbEmn);
+
+            var RTData = [
+                {"username":"张三", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"},
+                {"username":"李四", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"},
+                {"username":"王五", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"}
+            ];
+
+            table.render({
+                elem: '#RealTime',
+                data:RTData,
+                toolbar: '#toolbarDemo',
+                cols: [[
+                    {field:'id', width:'10%', title: '序号', type:'numbers'}
+                    ,{field:'username', width:'15%', title: '姓名'}
+                    ,{field:'sex', width:'15%', title: '进入时间'}
+                    ,{field:'city', width:'20%', title: '绿卡到期日期'}
+                    ,{field:'experience', width:'20%', title: '密卡到期日期'}
+                    ,{field:'score', width:'20%', title: 'CP到期日期'}
+                ]]
+                ,page: true
+            });
         }else if (data.context.id == "3"){
-            document.getElementById("page").innerHTML="3";
+            $("page").load("updateworker.html");
         }else if (data.context.id == "4"){
-            document.getElementById("page").innerHTML="4";
+            page.innerHTML="";
+            var html = '<script type="text/html" id="toolbarDemo">\n' +
+                '  <div class="layui-btn-container">\n' +
+                '    <button class="layui-btn layui-btn-sm" lay-event="getCheckData">获取选中行数据</button>\n' +
+                '  </div>\n' +
+                '</script>';
+            page.innerHTML = html;
+            var tbEmn = document.createElement("table");
+            tbEmn.className = "layui-hide";
+            tbEmn.id = "RealTime";
+            page.appendChild(tbEmn);
+
+            var RTData = [
+                {"username":"张三", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"},
+                {"username":"李四", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"},
+                {"username":"王五", "sex":"08:02:11","city":"2019-09-11","experience":"2019-01-10", "score":"2018-12-19"}
+            ];
+
+            table.render({
+                elem: '#RealTime',
+                data:RTData,
+                toolbar: '#toolbarDemo',
+                cols: [[
+                    {field:'id', width:'10%', title: '序号', type:'numbers'}
+                    ,{field:'username', width:'15%', title: '姓名'}
+                    ,{field:'sex', width:'15%', title: '进入时间'}
+                    ,{field:'city', width:'20%', title: '绿卡到期日期'}
+                    ,{field:'experience', width:'20%', title: '密卡到期日期'}
+                    ,{field:'score', width:'20%', title: 'CP到期日期'}
+                ]]
+                ,page: true
+            });
         }else if (data.context.id == "5"){
             document.getElementById("page").innerHTML="5";
         }
     });
 
-    table.render({
-        elem: '#test'
-        //,url:'/demo/table/user/'
-        ,cols: [[
-            {field:'id', width:80, title: 'ID', sort: true}
-            ,{field:'username', width:80, title: '用户名'}
-            ,{field:'sex', width:80, title: '性别', sort: true}
-            ,{field:'city', width:80, title: '城市'}
-            ,{field:'sign', title: '签名', minWidth: 150}
-            ,{field:'experience', width:80, title: '积分', sort: true}
-            ,{field:'score', width:80, title: '评分', sort: true}
-            ,{field:'classify', width:80, title: '职业'}
-            ,{field:'wealth', width:135, title: '财富', sort: true}
-        ]]
-        ,page: true
-    });
-
-    laypage.render({
-        elem: 'demo7'
-        ,count: 1000
-        ,layout: ['count', 'prev', 'page', 'next', 'limit', 'skip']
-        ,jump: function(obj){
-            console.log(obj)
-        }
-    });
-
+    // laypage.render({
+    //     elem: 'demo7'
+    //     ,count: 1000
+    //     ,layout: ['count', 'prev', 'page', 'next', 'limit', 'skip']
+    //     ,jump: function(obj){
+    //         console.log(obj)
+    //     }
+    // });
+    //
+    // //添加一行
+    // var oldData = table.cache["test"];
+    // var data1 = {"username": "dj"};
+    // oldData.push(data1);
+    // table.reload('test', {data: oldData});
+    //
+    // //删除CC这一行
+    // var oldData1 = table.cache["test"];
+    // oldData1.forEach(function (item, index) {
+    //     if (item.username == "cc")
+    //         oldData1.splice(index, 1);
+    // });
+    //
+    // table.reload('test', {data: oldData1});
 
     exports('main', {});
 });
